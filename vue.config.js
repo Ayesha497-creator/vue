50d5b83a56ddbdb6f853bccb1e00f6f560ced817
Ayesha Eman <eayesha507@gmail.com>
2025-12-29 17:31:42 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  // Vue CLI automatically loads .env files based on mode
-  // Use VUE_APP_BASE_URL from .env.[mode] files
-  publicPath: process.env.VUE_APP_BASE_URL || '/',
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080
-  }
-};
-
+  publicPath: process.env.VUE_APP_BASE_URL || '/'
+}
